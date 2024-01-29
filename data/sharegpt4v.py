7fcc29fa238fca1c73035cb6cf6f0810a3bb092f
ChenDelong1999 <673939938@qq.com>
2024-01-29 19:15:09 +0800

    implement multimodal LLM training on sharegpt

--- data/sharegpt4v.py
+++ data/sharegpt4v.py
@@ -7,9 +7,28 @@ from PIL import Image
 
 class ShareGPT4V(torch.utils.data.Dataset):
 
-    def __init__(self, annotation_file, dir_mapping):
+    def __init__(self, annotation_file, dir_mapping=None, only_return_img_path=False):
+
+        if dir_mapping is None:
+            dir_mapping = {
+                'sam/images': '/home/dchenbs/workspace/datasets/sa1b',
+                'coco/train2017': '/home/dchenbs/workspace/datasets/coco2017/images/train2017',
+                'llava/llava_pretrain/images': '/home/dchenbs/workspace/datasets/sharegpt4v/LLaVA-Pretrain',
+                'wikiart/images': '/home/dchenbs/workspace/datasets/sharegpt4v/WebData/wikiart/images',
+                'share_textvqa/images': '/home/dchenbs/workspace/datasets/sharegpt4v/WebData/share_textvqa/images',
+                'web-celebrity/images': '/home/dchenbs/workspace/datasets/sharegpt4v/WebData/web-celebrity/images',
+                    'Choi_Min-sik': 'Choi_Min_sik', 
+                    'Lee_Byung-hun': 'Lee_Byung_hun',
+                'web-landmark/images': '/home/dchenbs/workspace/datasets/sharegpt4v/WebData/web-landmark/images',
+                'vg/VG_100K': '/home/dchenbs/workspace/datasets/VisualGenome/VG_100K',
+                'vg/VG_100K_2': '/home/dchenbs/workspace/datasets/VisualGenome/VG_100K_2',
+                'gqa/images': '/home/dchenbs/workspace/datasets/gqa/images',
+                'ocr_vqa/images': '/home/dchenbs/workspace/datasets/sharegpt4v/ocr_vqa/images',
+                'textvqa/train_images': '/home/dchenbs/workspace/datasets/sharegpt4v/textvqa/train_images',
+            }
 
         self.dir_mapping = dir_mapping
+        self.only_return_img_path = only_return_img_path
 
         samples = json.load(open(annotation_file, 'r'))
         self.samples = [s for s in samples if 'image' in s]
@@ -38,6 +57,13 @@ class ShareGPT4V(torch.utils.data.Dataset):
         return failed_samples
 
 
+    def process_sharegpt4v_sample(self, img_path, sample, start_path='<img_path>', end_path='</img_path>', human_turn='### Human: \n', gpt_turn='### AI: \n', eos_token='<|endoftext|>'):
+        result = ''
+        for utterance in sample['conversations']:
+            result += human_turn if utterance['from']=='human' else gpt_turn
+            result += utterance['value'].replace('<image>', f'{start_path}{img_path}{end_path}') + eos_token + '\n'
+        return result
+
     def __getitem__(self, index):
         sample = self.samples[index]
         img_path = sample['image']
@@ -52,7 +78,10 @@ class ShareGPT4V(torch.utils.data.Dataset):
         # img = Image.open(img_path)
         assert os.path.exists(img_path), f'Image not found: {img_path}'
 
-        return img_path, sample
+        if self.only_return_img_path:
+            return img_path
+        else:
+            return self.process_sharegpt4v_sample(img_path, sample)
     
     def __len__(self):
         return len(self.samples)
--- model/modeling_multimodal_phi2.py
+++ model/modeling_multimodal_phi2.py
@@ -1,8 +1,10 @@
 import torch
 import torchvision
 import numpy as np
-
+from PIL import Image
 from transformers.utils import ModelOutput
+import time
+import pprint
 
 from .phi import PhiForCausalLM, PhiConfig
 from .phi.modeling_phi import *
@@ -100,6 +102,7 @@ class MultimodalCausalLMOutputWithPast(ModelOutput):
     attentions: Optional[Tuple[torch.FloatTensor]] = None
     predicted_segment_latents: Optional[torch.FloatTensor] = None
     predicted_segment_bboxes: Optional[torch.FloatTensor] = None
+    profiling: Optional[dict] = None
     
 
 class PhiForMultimodal(PhiForCausalLM):
@@ -108,7 +111,9 @@ class PhiForMultimodal(PhiForCausalLM):
             self,
             config: PhiConfig,
             w_segment_loss: float = 1.0,
-            w_bbox_loss: float = 1.0,
+            w_bbox_loss: float = 1/1000,
+            seqae_batch_size: Optional[int] = 128,
+                # batch size for seqae inference, -1 for full segments inference for each sample
             ) -> None:
         super().__init__(config)
 
@@ -120,6 +125,7 @@ class PhiForMultimodal(PhiForCausalLM):
 
         self.w_segment_loss=w_segment_loss
         self.w_bbox_loss=w_bbox_loss
+        self.seqae_batch_size = seqae_batch_size
 
         self.post_init()
 
@@ -136,84 +142,14 @@ class PhiForMultimodal(PhiForCausalLM):
         self.segment_head = SegmentHead(self.config.n_embd, self.seqae.config.d_latent).to(self.device)
         self.seqae_loaded = True
 
-    def preprocess_segments(self, segment_masks, images):
-
-        def resize(segment):
-            # segment['patch'] is PIL Image object
-            w, h = segment['patch'].size
-            if h * w > (self.seqae.config.data_seq_length):
-                ratio_to_maxlength = np.sqrt(self.seqae.config.data_seq_length / (h * w))
-                h = int(h * ratio_to_maxlength)
-                w = int(w * ratio_to_maxlength)
-                segment['patch'] = torchvision.transforms.Resize([h, w])(segment['patch'])
-                segment['segmentation'] = torchvision.transforms.Resize([h, w])(segment['segmentation'][None, :, :])[0]
-
-            return segment
-        
-        def encode_to_sequence(segment):
-            # segment['patch'] is torch tensor with shape (C, H, W)
-            h, w = segment['patch'].shape[1:]
-            sequence = []
-            for i in range(h):
-                for j in range(w):
-                    pixel_data = segment['patch'][:, i, j] / 255.0
-                    is_rightmost = 1 if j == w - 1 else 0
-                    is_non_masked = int(segment['segmentation'][i, j])
-                    sequence.append(pixel_data.tolist() + [is_rightmost, is_non_masked])
-            sequence = np.array(sequence) 
-
-            # pad the sequence to max_seq_length with zeros
-            if len(sequence) < self.seqae.config.data_seq_length:
-                sequence = np.concatenate((sequence, np.zeros((self.seqae.config.data_seq_length - len(sequence), self.seqae.config.input_channels))))
-
-            # add the query place holder to the end of the sequence
-            sequence = np.concatenate((sequence, np.zeros((self.seqae.config.num_queries, self.seqae.config.input_channels))))
-            # add one all zero column to the start 
-            sequence = np.concatenate((np.zeros((1, sequence.shape[1])), sequence), axis=0)
-
-            return torch.from_numpy(sequence)
-    
-        segment_sequences = []
-        bboxes = []
-        for segment_mask in segment_masks:
-            image = images[segment_mask["image_index"]]
-
-            mask = segment_mask["segmentation"]
-            bbox = segment_mask["bbox"]
-            bbox[2] = 1 if bbox[2] == 0 else bbox[2]
-            bbox[3] = 1 if bbox[3] == 0 else bbox[3]
-            x, y, w, h = bbox
-
-            segment = {
-                "patch": image.crop((x, y, x + w, y + h)),
-                "segmentation": mask[y:y+h, x:x+w],
-            }
-
-            segment = resize(segment)
-            segment['patch'] = torchvision.transforms.ToTensor()(segment['patch'])
-            segment['patch'] = segment['patch'] * segment['segmentation'][None, :, :]
-            segment_sequence = encode_to_sequence(segment)
-            segment_sequence = segment_sequence.to(self.device)
-            segment_sequences.append(segment_sequence)
-            bboxes.append(bbox)
-
-        segment_sequences = torch.stack(segment_sequences, dim=0).to(self.device, dtype=self.dtype)
-        bboxes = torch.tensor(bboxes).to(self.device, dtype=self.dtype)
-        return segment_sequences, bboxes
-
     def forward(
         self,
         input_ids: torch.LongTensor,
-        segment_masks: Optional[list] = None,
-            # A list of batch_size number of list of dicts, whose key includes 
-            #   - "image_index": int
-            #   - "mask": binary mask of torch.Tensor with shape (H, W)
-            #   - "bbox": bounding box of the object in the image (x, y, w, h)
-            # if no segment token in certain sample, the corresponding list should be empty
-        images: Optional[list] = None,
-            # A list of PIL Images objects
-        seqae_batch_size: Optional[int] = 16,
-            # batch size for seqae inference, -1 for full segments inference for each sample
+        segment_sequences: list,
+            # a list of num `batch_size` elements, each element is a tensor of [image_num, num_segments, seq_length, input_channels]
+        bboxes: list,
+            # a list of num `batch_size` elements, each element is a tensor of [image_num, num_segments, 4] representing bounding boxes in x, y, w, h
+
         past_key_values: Optional[Union[torch.FloatTensor, InferenceParams]] = None,
         attention_mask: Optional[torch.BoolTensor] = None,
         labels: Optional[torch.LongTensor] = None,
@@ -223,44 +159,50 @@ class PhiForMultimodal(PhiForCausalLM):
         if not self.seqae_loaded:
             raise ValueError("Please load seqae model first.")
         
-        if segment_masks is not None:
-            batch_segment_tokens = []
-            batch_segment_bboxes = []
-            batch_segment_latents = []
-            for sample_segment_masks in segment_masks:
-                if len(sample_segment_masks)!=0:
-                    segment_sequences, bboxes = self.preprocess_segments(sample_segment_masks, images)
+        profiling = {}        
+        step_start = time.time()
+        start = time.time()
 
-                    if seqae_batch_size != -1:
-                        segment_latents = []
-                        for i in range(0, len(segment_sequences), seqae_batch_size):
-                            segment_latents.append(self.seqae.encode(segment_sequences[i:i+seqae_batch_size]))
-                        segment_latents = torch.cat(segment_latents, dim=0)
-                    else:
-                        segment_latents = self.seqae.encode(segment_sequences)
+        bs = input_ids.shape[0]
 
-                    latents_and_bboxes = torch.cat((segment_latents, bboxes), dim=1)
-                    segment_tokens = self.visual_token_embedding(latents_and_bboxes)
-                    batch_segment_tokens.append(segment_tokens)
-                    batch_segment_bboxes.append(bboxes)
-                    batch_segment_latents.append(segment_latents)
+        # segment embedding
+        batch_segment_tokens = []
+        batch_segment_bboxes = []
+        batch_segment_latents = []
+        batch_segment_position_ids = []
+        for sample_idx in range(bs):
+            # if there is segments in this sample
+            if len(segment_sequences[sample_idx]) != 0: 
+                # flatten the image_num axis, in case there are multiple images in one sample
+                sample_segment_sequences = segment_sequences[sample_idx].flatten(start_dim=0, end_dim=1).to(self.device)
+                sample_bboxes = bboxes[sample_idx].flatten(start_dim=0, end_dim=1).to(self.device, dtype=self.dtype)
+                if self.seqae_batch_size != -1:
+                    segment_latents = []
+                    for i in range(0, len(sample_segment_sequences), self.seqae_batch_size):
+                        segment_latents.append(self.seqae.encode(sample_segment_sequences[i:i+self.seqae_batch_size]))
+                    segment_latents = torch.cat(segment_latents, dim=0)
                 else:
-                    batch_segment_tokens.append([])
-                    batch_segment_bboxes.append([])
-                    batch_segment_latents.append([])
-                
+                    segment_latents = self.seqae.encode(sample_segment_sequences)
+
+                latents_and_bboxes = torch.cat((segment_latents, sample_bboxes), dim=1)
+                segment_tokens = self.visual_token_embedding(latents_and_bboxes)
+                batch_segment_tokens.append(segment_tokens)
+                batch_segment_bboxes.append(sample_bboxes)
+                batch_segment_latents.append(segment_latents)
+            else:
+                batch_segment_tokens.append([])
+                batch_segment_bboxes.append([])
+                batch_segment_latents.append([])
+
             # find the position of <|seg|> in input_ids
-            batch_segment_position_ids = []
-            for i in range(len(input_ids)):
-                seg_pos = torch.where(input_ids[i] == self.special_token_id_mappinmg["<|seg|>"])[0].tolist()
-                assert len(seg_pos) == len(segment_masks[i]), f"number of <|seg|> in input_ids ({len(seg_pos)}) does not match number of segments ({len(segment_masks[i])})"
-                batch_segment_position_ids.append(seg_pos)
-        else:
-            batch_segment_tokens = None
-            batch_segment_bboxes = None
-            batch_segment_latents = None
-            batch_segment_position_ids = None
-            
+            # seg_pos = torch.where(input_ids[sample_idx] == self.special_token_id_mappinmg["<|seg|>"])[0].tolist()
+            seg_pos = torch.where(torch.tensor(input_ids[sample_idx]) == torch.tensor(self.special_token_id_mappinmg["<|seg|>"]))[0].tolist()
+            assert len(seg_pos) == len(sample_segment_sequences), f"In the {sample_idx}th sample, number of <|seg|> in input_ids ({len(seg_pos)}) does not match number of segments ({len(sample_segment_sequences)})"
+            batch_segment_position_ids.append(seg_pos)
+
+        profiling['[time]/embed segments'] = time.time() - start
+        start = time.time()
+
         hidden_states = self.transformer(
             input_ids, 
             segment_tokens=batch_segment_tokens,
@@ -270,32 +212,46 @@ class PhiForMultimodal(PhiForCausalLM):
             )
         lm_logits = self.lm_head(hidden_states)
 
+        profiling['[time]/LLM'] = time.time() - start
+        start = time.time()
+
         # LLM Transformer last hidden state predicts next text token
         loss = None
         lm_loss = None
-        if labels is not None:
-            lm_loss = self.lm_loss(lm_logits, labels)
+        if labels is None:
+            # autoregressive modelling: labels is shifted to the right by one based on input_ids
+            labels = input_ids[..., 1:].contiguous()
+            lm_logits = lm_logits[..., :-1, :].contiguous()
+
+        lm_loss = self.lm_loss(lm_logits, labels)
 
         # LLM Transformer last hidden state predicts segment latent and bbox
         segment_loss = 0
         bbox_loss = 0
-        if segment_masks is not None:
-            predicted_segment_latents = []
-            predicted_segment_bboxes = []
-            for i in range(len(batch_segment_tokens)):
-                if len(batch_segment_tokens[i]) != 0:
-                    segment_latents, segment_bboxes = self.segment_head(hidden_states[i, batch_segment_position_ids[i]])
-                    segment_loss += self.segment_loss(segment_latents, batch_segment_latents[i])
-                    bbox_loss += self.segment_loss(segment_bboxes, batch_segment_bboxes[i])
+        predicted_segment_latents = []
+        predicted_segment_bboxes = []
+        for sample_idx in range(len(batch_segment_tokens)):
+            if len(batch_segment_tokens[sample_idx]) != 0:
+                segment_latents, segment_bboxes = self.segment_head(hidden_states[sample_idx, batch_segment_position_ids[sample_idx]])
+                segment_loss += self.segment_loss(segment_latents, batch_segment_latents[sample_idx])
+                bbox_loss += self.segment_loss(segment_bboxes, batch_segment_bboxes[sample_idx])
 
-                    predicted_segment_latents.append(segment_latents)
-                    predicted_segment_bboxes.append(segment_bboxes)
-                else:
-                    predicted_segment_latents.append([])
-                    predicted_segment_bboxes.append([])
-        else:
-            predicted_segment_latents = None
-            predicted_segment_bboxes = None
+                predicted_segment_latents.append(segment_latents)
+                predicted_segment_bboxes.append(segment_bboxes)
+            else:
+                predicted_segment_latents.append([])
+                predicted_segment_bboxes.append([])
+            
+        profiling['[time]/segment head'] = time.time() - start
+        profiling['[time]/total'] = time.time() - step_start
+
+        profiling['[loss]/lm_loss'] = lm_loss.item()
+        profiling['[loss]/segment_loss'] = segment_loss.item()
+        profiling['[loss]/bbox_loss'] = bbox_loss.item()
+
+        # print('-'*80)
+        # for k, v in profiling.items():
+        #     print(f"\t{k}:\t{v}")
 
         loss = lm_loss + self.w_segment_loss * segment_loss + self.w_bbox_loss * bbox_loss
 
@@ -306,5 +262,6 @@ class PhiForMultimodal(PhiForCausalLM):
             hidden_states=hidden_states,
             predicted_segment_latents=predicted_segment_latents,
             predicted_segment_bboxes=predicted_segment_bboxes,
+            profiling=profiling,
             )
     
