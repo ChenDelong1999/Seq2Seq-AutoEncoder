5bfdddc468a86f1e5bc5474fcd8e55ebec00e71c
ChenDelong1999 <673939938@qq.com>
2024-02-07 11:30:25 +0800

    delete phi legacy code

--- model/phi_legacy/__init__.py
+++
@@ -1,4 +0,0 @@
-# This folder is copied from https://huggingface.co/microsoft/phi-2/tree/main
-
-from .configuration_phi import PhiConfig
-from .modeling_phi import PhiForCausalLM, PhiModel, PhiPreTrainedModel
